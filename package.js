6d63af40b2d01ee0b44a7fa5f1d490a882dfdfa7
Androo <loneleeandro.o@gmail.com>
2015-06-15 13:29:27 +1000

    fixed Safari compatability

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'loneleeandroo:polymerize',
-  version: '0.3.1',
+  version: '0.3.2',
   summary: 'Synthesises Polymer and Meteor',
   git: 'https://github.com/loneleeandroo/meteor-polymerize',
   documentation: 'README.md'
